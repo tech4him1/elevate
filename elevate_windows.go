e6ebdf98202c5899e21699f6c524efa02f650a06
Caleb <inchristalway-tech4him@yahoo.com>
2017-02-16 15:44:20 -0700

    Go Fmt everything.

--- elevate_windows.go
+++ elevate_windows.go
@@ -1,28 +1,28 @@
 package elevate
 
 import (
-    "log"
-    "golang.org/x/sys/windows"
-    "unsafe"
+	"golang.org/x/sys/windows"
+	"log"
+	"unsafe"
 )
 
 func Elevate(exePath, params, workDir string) uint {
-    shell32 := windows.NewLazyDLL("shell32.dll")
-    shellExecuteW := shell32.NewProc("ShellExecuteW")
+	shell32 := windows.NewLazyDLL("shell32.dll")
+	shellExecuteW := shell32.NewProc("ShellExecuteW")
 
-    returnCode, _, _ := shellExecuteW.Call(0,
-        uintptr(unsafe.Pointer(strToNullStr("runas"))),
-        uintptr(unsafe.Pointer(strToNullStr(exePath))),
-        uintptr(unsafe.Pointer(strToNullStr(params))),
-        uintptr(unsafe.Pointer(strToNullStr(workDir))),
-        1)
-    return uint(returnCode)
+	returnCode, _, _ := shellExecuteW.Call(0,
+		uintptr(unsafe.Pointer(strToNullStr("runas"))),
+		uintptr(unsafe.Pointer(strToNullStr(exePath))),
+		uintptr(unsafe.Pointer(strToNullStr(params))),
+		uintptr(unsafe.Pointer(strToNullStr(workDir))),
+		1)
+	return uint(returnCode)
 }
 
 func strToNullStr(s string) *uint16 {
-    ptr, err := windows.UTF16PtrFromString(s)
-    if err != nil {
-        log.Print(err)
-    }
-    return ptr
+	ptr, err := windows.UTF16PtrFromString(s)
+	if err != nil {
+		log.Print(err)
+	}
+	return ptr
 }
